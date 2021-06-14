ef011c0100fc207e5ae80447263f9f20aaa66b64
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-06-14 18:21:19 +0900

    Revert "front: cors"
    This reverts commit 094b0975a77b167d2977bd49e951cce2373341ef.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
-
+import React from 'react';
 
 const index = () => {
-
-    const [message, setMessage] = useState("test");
-
-    const onClick = useCallback(async () => {
-        const result = await axios.get('http:localhost:8080/hello');        
-        setMessage(result.data);
-    }, []);
-
-
     return (
         <div>
-            <div>{message}</div>
-            <button onClick={onClick}>button!!</button>
+            <div>index</div>
         </div>
     )
 }
