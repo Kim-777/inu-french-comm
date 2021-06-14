094b0975a77b167d2977bd49e951cce2373341ef
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-06-14 18:02:08 +0900

    front: cors

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
 
 const index = () => {
+
+    const [message, setMessage] = useState("test");
+
+    const onClick = useCallback(async () => {
+        const result = await axios.get('http:localhost:8080/hello');        
+        setMessage(result.data);
+    }, []);
+
+
     return (
         <div>
-            <div>index</div>
+            <div>{message}</div>
+            <button onClick={onClick}>button!!</button>
         </div>
     )
 }
